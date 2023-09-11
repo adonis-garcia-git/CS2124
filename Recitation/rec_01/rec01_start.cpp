99ce26fafda0af9f5e39186801abc0551d5b6454
adonis-garcia-git <adongarc5@gmail.com>
2023-09-11 09:40:28 -0400

    new

--- Recitation/rec_01/rec01_start.cpp
+++ Recitation/rec_01/rec01_start.cpp
@@ -4,6 +4,10 @@
   jbs
 */
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
 //
 // main
 //
@@ -17,7 +21,6 @@ int main() { // Yes, it has to have an int for the return type
 
 
     // Task 2  Printing hello ...  Using "using namespace"
-    using namespace std;
     cout << "hello" << '\n';
 
 
@@ -87,37 +90,98 @@ int main() { // Yes, it has to have an int for the return type
       cout << k << " ";
       k++;
     } while (k <= 20);
+    cout << endl;
 
 
     // Task 8  Looping to successfully open a file, asking user for the name
-    
+    ifstream ifs;
+    string name;
+    do {
+      cout << "File name: ";
+      cin >> name;
+      ifs.open(name);
+    } while (!ifs);
 
     // Task 9  Looping, reading file word by "word".
-
+    string word;
+    while (ifs >> word)
+    {
+      cout << word << '\n';
+    }
+    ifs.close();
 
     // Task 10 Open a file of integers, read it in, and display the sum.
+    ifstream int_file("integers.txt");
+    int sum{};
+    int num{};
+    if (!int_file)
+    {
+      cerr << "Could not open file\n";
+      exit(1);
+    }
+    while (int_file >> num)
+    {
+      sum += num;
+    }
+    cout << "The sum is: " << sum << '\n';
+    int_file.close();
 
 
-    // Taks 11 Open and read a file of integers and words. Display the sum.
-
+    // Task 11 Open and read a file of integers and words. Display the sum.
+    ifstream mixed_file("mixed.txt");
+    int sum2{};
+    int num2{};
+    if (!mixed_file)
+    {
+      cerr << "Could not open file\n";
+      exit(1);
+    }
+    while (mixed_file >> num2)
+    {
+      sum2 += num2;
+    }
+    cout << "The sum is: " << sum2 << '\n';
+    mixed_file.close(); //reading loop ends once it reaches a non int
 
     //
     // Vectors
     //
 
     // Task 12 Filling a vector of ints
-
+    vector<int> vectorOfInts;
+    for (int i{10}; i <= 100; i+=2)
+    {
+      vectorOfInts.push_back(i);
+    }
     
     // Task 13 Displaying the vector THREE times
+
     //         a) using the indices,
+    for (size_t i{}; i < vectorOfInts.size(); i++)
+    {
+      cout << vectorOfInts[i] << " ";
+    }
+    cout << endl;
 
     //         b) using a "ranged for"
+    for (int i: vectorOfInts)
+    {
+      cout << i << " ";
+    }
+    cout << endl;
 
     //         c) using indices again but backwards
-
+    for (size_t i{vectorOfInts.size()}; i > 0; i--)
+    {
+      cout << vectorOfInts[i-1] << " ";
+    }
+    cout << endl;
 
     // Task 14. Initialize a vector with the primes less than 20. 
-
-    
+    vector<int> primesVector{2, 3, 5, 7, 11, 13, 17, 19};
+    for (int i: primesVector)
+    {
+      cout << i << " ";
+    }
 }
 
